cfadd8c2343bf19c437e65a747bd8d57cb2ae4d4
André Kuratomi <andrekuratomi@gmail.com>
2021-10-29 20:23:59 -0300

    feat: ninja-model updated to step 3

--- src/models/ninja_model.py
+++ src/models/ninja_model.py
@@ -15,22 +15,7 @@ class Ninja:
 
     def learn_jutsu(self, jutsu):
         self.jutsu_list.append(jutsu)
-        return f"O ninja {self.name} {self.clan} acabou de aprender um novo jutsu: {self.village}"
-
-    @staticmethod
-    def check_health(ninja_to_check):
-        if ninja_to_check.health_pool < 0:
-            ninja_to_check.health_pool = 0
-            ninja_to_check.concious = False
-        return ninja_to_check.concious
-
-    def cast_jutsu(self, jutsu, adversary):
-        if adversary.concious is False:
-            return False
-        if jutsu in self.jutsu_list and self.chakra_pool >= 0:
-            adversary.health_pool = jutsu.jutsu_damage
-            self.chakra_pool -= jutsu.chakra_spend
-            return True
+        return f"O ninja {self.name} {self.clan} acabou de aprender um novo jutsu: {self.village.capitalize()}"
 
 
 naruto = Ninja('Naruto', 'Uzumaki', 'Konoha')
@@ -47,12 +32,4 @@ res = naruto.learn_jutsu(rasengan)
 print(ser)
 print(res)
 
-rasengan = Jutsu('Rasengan', 'Vento', 'a', 20, -15)
-naruto = Ninja('Naruto', 'Uzumaki', 'Konoha')
-
-naruto.learn_jutsu(rasengan)
-
-sasuke = Ninja('Sasuke', 'Uchiha', 'Konoha')
-
-res5 = naruto.cast_jutsu(rasengan, sasuke)
-print(res5)
+# > 'O ninja Naruto Uzumaki acabou de aprender um novo jutsu: Rasengan'
