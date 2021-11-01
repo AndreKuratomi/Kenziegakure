2dac9d9aa2a9c97da54d820324199ca6583fdf2f
André Kuratomi <andrekuratomi@gmail.com>
2021-11-01 18:12:26 -0300

    fix: code fixed to 14/15 tests pass

--- src/models/jounin_model.py
+++ src/models/jounin_model.py
@@ -1,13 +1,12 @@
-from .ninja_model import Ninja
+from src.models.ninja_model import Ninja
 
 
 class Jounin(Ninja):
-    #?
-    # super(ninja_level).__init__(ninja_level)
+
     ninja_level = "Jounin"
 
-    def __init__(self, name: str, clan: str, village: str, proficiency: dict = {'taijutsu': int, 'ninjutsu': int, 'genjutsu': int}, in_this_mission: bool = False):
-        super().__init__(name, clan, village)
+    def __init__(self, name: str, clan: str, village: str, proficiency: dict, in_this_mission: bool = False):
+        super().__init__(name, clan, village, ninja_level=self.ninja_level)
         self.proficiency = proficiency
         self.is_in_mission = in_this_mission
 
@@ -27,14 +26,14 @@ class Jounin(Ninja):
     def start_mission(self):
         if self.is_in_mission is False:
             self.is_in_mission = True
-            return f'O ninja {self.name} {self.clan} já está em uma missão'
-        return f'O ninja {self.name} {self.clan} saiu em missão'
+            return f'O ninja {self.name} {self.clan} saiu em missão'
+        return f'O ninja {self.name} {self.clan} já está em uma missão'
 
     def return_from_mission(self):
         if self.is_in_mission is True:
             self.is_in_mission = False
-            return f'O ninja {self.name} {self.clan} não está em nenhuma missão no momento'
-        return f'O ninja {self.name} {self.clan} retornou em segurança da missão'
+            return f'O ninja {self.name} {self.clan} retornou em segurança da missão'
+        return f'O ninja {self.name} {self.clan} não está em nenhuma missão no momento'
 
 
 # Atributo de classe:
@@ -58,3 +57,6 @@ print(res3)
 
 res4 = kakashi.return_from_mission()
 print(res4)   
+
+joh = Jounin.ninja_level
+print(joh)
--- src/models/ninja_model.py
+++ src/models/ninja_model.py
@@ -1,14 +1,14 @@
-from .jutsu_model import Jutsu
+from src.models.jutsu_model import Jutsu
 
 
 class Ninja:
 
-    def __init__(self, name: str, clan: str, village: str, ninja_level: str = "Unranked", jutsu_list: list = [], health_pool: int = 100, chakra_pool: int = 100, concious: bool = True) -> None:
+    def __init__(self, name: str, clan: str, village: str, ninja_level: str = "Unranked", health_pool: int = 100, chakra_pool: int = 100, concious: bool = True) -> None:
         self.name = name
         self.clan = clan
         self.village = village
         self.ninja_level = ninja_level
-        self.jutsu_list = jutsu_list
+        self.jutsu_list = []
         self.health_pool = health_pool
         self.chakra_pool = chakra_pool
         self.concious = concious
@@ -16,6 +16,23 @@ class Ninja:
     def learn_jutsu(self, jutsu):
         self.jutsu_list.append(jutsu)
         return f"O ninja {self.name} {self.clan} acabou de aprender um novo jutsu: {self.village.capitalize()}"
+
+    @staticmethod
+    def check_health(ninja_to_check):
+        if ninja_to_check.health_pool < 0:
+            ninja_to_check.health_pool = 0
+            ninja_to_check.concious = False
+        return ninja_to_check.concious
+
+    def cast_jutsu(self, jutsu, adversary):
+        if adversary.concious is not True:
+            return False
+        if jutsu in self.jutsu_list and self.chakra_pool >= 100:
+            adversary.health_pool -= jutsu.jutsu_damage
+            self.chakra_pool -= jutsu.chakra_spend
+            return True
+        # elif adversary.health_pool >= 100 and self.chakra_pool >= 100:
+        return False
 
 
 naruto = Ninja('Naruto', 'Uzumaki', 'Konoha')
@@ -27,9 +44,22 @@ alguem = Jutsu('Alguem', 'Terra', 'm', 20, 10)
 naruto = Ninja('Naruto', 'Uzumaki', 'Konoha')
 oturan = Ninja('Naruto', 'Uzumaki', 'donoha')
 
-ser = oturan.learn_jutsu(alguem)
-res = naruto.learn_jutsu(rasengan)
+ser = oturan.learn_jutsu(rasengan)
+res = naruto.learn_jutsu(alguem)
+naruto.health_pool = 100
+ers = Ninja.check_health(naruto)
 print(ser)
 print(res)
+print(ers)
 
 # > 'O ninja Naruto Uzumaki acabou de aprender um novo jutsu: Rasengan'
+
+rasengan = Jutsu('Rasengan', 'Vento', 'a', 20, -15)
+naruto = Ninja('Naruto', 'Uzumaki', 'Konoha')
+
+naruto.learn_jutsu(rasengan)
+
+sasuke = Ninja('Sasuke', 'Uchiha', 'Konoha')
+
+res = naruto.cast_jutsu(rasengan, sasuke)
+print(res)
